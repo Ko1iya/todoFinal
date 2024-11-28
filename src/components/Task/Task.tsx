6f0cde59af7ea580712f83e96d7131709e5ebc42
Ko1iya <kolya.fialkovskiy@gmail.com>
2024-11-28 14:14:33 +0800

    timer ready

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { formatDistanceToNow } from 'date-fns';
 import styles from './task.module.scss';
 
 interface TaskProps {
   children: string;
-  taskState?: string;
+  taskState: string;
   toggleTaskStateCompleted: (id: number) => void;
   toggleTaskStateEditing: (id: number) => void;
   id: number;
@@ -20,95 +20,168 @@ interface TaskProps {
   ) => void;
 }
 
-function Task(prop: TaskProps) {
-  // Использую библиотеку date-fns для получения прошедшего времени в строковом формате
-  const timeCreated = formatDistanceToNow(new Date(), {
-    includeSeconds: true,
-  });
+interface TaskState {
+  timePassed: string;
+  timeCreated: Date;
+  timeTimer: number;
+}
 
-  const {
-    children,
-    taskState,
-    id,
-    toggleTaskStateCompleted,
-    deleteTask,
-    changeContent,
-    toggleTaskStateEditing,
-    toggleEditingToActive,
-  } = prop;
+class Task extends Component<TaskProps, TaskState> {
+  static formatSeconds(seconds: number): string {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
 
-  return (
-    <li className={taskState ? styles[taskState] : undefined}>
-      {taskState !== 'editing' ? (
-        <div className={styles.view}>
-          <input
-            className={styles.toggle}
-            type="checkbox"
-            checked={taskState === 'completed'}
-            onChange={() => {
-              toggleTaskStateCompleted(id);
-            }}
-          />
-          <label htmlFor="input">
-            <span
-              className={styles.title}
-              onClick={() => {
+    if (hours > 0) {
+      return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    }
+
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+  }
+
+  private refTimeCreate: ReturnType<typeof setInterval>;
+
+  private refTimeTimer: ReturnType<typeof setInterval>;
+
+  constructor(props: TaskProps) {
+    super(props);
+    this.state = {
+      timeCreated: new Date(),
+      timePassed: '',
+      timeTimer: 0,
+    };
+  }
+
+  componentDidMount(): void {
+    const { timeCreated } = this.state;
+
+    this.setState({
+      timePassed: formatDistanceToNow(timeCreated, {
+        includeSeconds: true,
+      }),
+    });
+
+    this.refTimeCreate = setInterval(() => {
+      this.setState({
+        timePassed: formatDistanceToNow(timeCreated, {
+          includeSeconds: true,
+        }),
+      });
+    }, 5000);
+  }
+
+  componentWillUnmount(): void {
+    clearInterval(this.refTimeCreate);
+    clearInterval(this.refTimeTimer);
+  }
+
+  toggleTimer(stateTimer: boolean): void {
+    if (stateTimer) {
+      clearInterval(this.refTimeTimer);
+
+      return;
+    }
+
+    this.refTimeTimer = setInterval(() => {
+      this.setState((prevstate) => ({
+        timeTimer: prevstate.timeTimer + 1,
+      }));
+    }, 1000);
+  }
+
+  render() {
+    const { timePassed, timeTimer } = this.state;
+
+    const {
+      children,
+      taskState,
+      id,
+      toggleTaskStateCompleted,
+      deleteTask,
+      changeContent,
+      toggleTaskStateEditing,
+      toggleEditingToActive,
+    } = this.props;
+
+    return (
+      <li className={taskState ? styles[taskState] : undefined}>
+        {taskState !== 'editing' ? (
+          <div className={styles.view}>
+            <input
+              className={styles.toggle}
+              type="checkbox"
+              checked={taskState === 'completed'}
+              onChange={() => {
                 toggleTaskStateCompleted(id);
               }}
-              aria-hidden="true"
-            >
-              {children}
-            </span>
-            <span className={styles.description}>
-              <button
-                aria-label="play"
-                type="button"
-                className={[styles.icon, styles.iconPlay].join(' ')}
-              />
-              <button
-                type="button"
-                className={[styles.icon, styles.iconPause].join(' ')}
-                aria-label="pause"
-              />
-              12:25
-            </span>
-            <span className={styles.description}>created {timeCreated}</span>
-          </label>
-          <button
-            type="button"
-            className={`${styles.icon} ${styles.iconEdit}`}
-            onClick={() => {
+            />
+            <label htmlFor="input">
+              <span
+                className={styles.title}
+                onClick={() => {
+                  toggleTaskStateCompleted(id);
+                }}
+                aria-hidden="true"
+              >
+                {children}
+              </span>
+              <span className={styles.description}>
+                <button
+                  aria-label="play"
+                  type="button"
+                  className={[styles.icon, styles.iconPlay].join(' ')}
+                  onClick={() => {
+                    this.toggleTimer(false);
+                  }}
+                />
+                <button
+                  type="button"
+                  className={[styles.icon, styles.iconPause].join(' ')}
+                  aria-label="pause"
+                  onClick={() => {
+                    this.toggleTimer(true);
+                  }}
+                />
+                {Task.formatSeconds(timeTimer)}
+              </span>
+              <span className={styles.description}>created {timePassed}</span>
+            </label>
+            <button
+              type="button"
+              className={`${styles.icon} ${styles.iconEdit}`}
+              onClick={() => {
+                toggleTaskStateEditing(id);
+              }}
+              aria-label="edit"
+            />
+            <button
+              type="button"
+              className={`${styles.icon} ${styles.iconDestroy}`}
+              onClick={() => {
+                deleteTask(id);
+              }}
+              aria-label="delete"
+            />
+          </div>
+        ) : (
+          <input
+            type="text"
+            className={styles.edit}
+            value={children}
+            onChange={(e) => {
+              changeContent(e, id);
+            }}
+            onKeyDown={(e) => {
+              toggleEditingToActive(e, id);
+            }}
+            onBlur={() => {
               toggleTaskStateEditing(id);
             }}
-            aria-label="edit"
           />
-          <button
-            type="button"
-            className={`${styles.icon} ${styles.iconDestroy}`}
-            onClick={() => {
-              deleteTask(id);
-            }}
-            aria-label="delete"
-          />
-        </div>
-      ) : (
-        <input
-          type="text"
-          className={styles.edit}
-          value={children}
-          onChange={(e) => {
-            changeContent(e, id);
-          }}
-          onKeyDown={(e) => {
-            toggleEditingToActive(e, id);
-          }}
-          onBlur={() => {
-            toggleTaskStateEditing(id); // Здесь добавлен обработчик события onBlur
-          }}
-        />
-      )}
-    </li>
-  );
+        )}
+      </li>
+    );
+  }
 }
 
 export default Task;
